afdf93eb08d48360814a3184e1027ea450e85697
jeremykemp7 <jkemp@lucidbots.com>
2025-03-07 11:48:27 -0500

    added amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ecm8ec352cd": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "Media": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
